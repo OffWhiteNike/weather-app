52348b69317c71121e1e93667958821178e41b03
OffWhiteNike <42612800+OffWhiteNike@users.noreply.github.com>
2018-09-17 21:44:06 +0900

    weatherapp

---
+++ Weather.js
@@ -0,0 +1,119 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View,} from 'react-native';
+import { LinearGradient } from "expo";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import PropTypes from "prop-types";
+
+const weatherCases = {
+    Rain: {
+        colors:["#00C6FB", "#005BEA"],
+        title: "It's raining outside!",
+        subtitle: "Don't forget your umbrella",
+        icon: 'weather-pouring'
+    },
+    Clear: {
+        colors:["#FEF253", "#FF7300"],
+        title: "Sunny outside!",
+        subtitle: "Enjoy your walk ;-)",
+        icon: 'weather-sunny'
+    },
+    Thunderstorm: {
+        colors:["#00ECBC", "#007ADF"],
+        title: "Thunderstorm is coming...",
+        subtitle: "It's safe to stay inside",
+        icon: 'weather-lightning'
+    },
+    Clouds: {
+        colors:["#D7D2CC", "#304352"],
+        title: "I know it's gloomy",
+        subtitle: "Wanna see crystal clear sky :-(",
+        icon: 'weather-cloudy'
+    },
+    Snow: {
+        colors:["#7DE2FC", "#B9B6E5"],
+        title: "Are you there? Elsa!?",
+        subtitle: "Do you want to build a snowman☃?",
+        icon: 'weather-snowy'
+    },
+    Drizzle: {
+        colors:["#89F7FE", "#66A6FF"],
+        title: "Drizzling all the way down~",
+        subtitle: "How about Macchiato with drizzling caramel?",
+        icon: 'weather-rainy'
+    },
+    Mist: {
+        colors:["#D7D2CC", "#304352"],
+        title: "Wet load...",
+        subtitle: "Becareful! While you are driving",
+        icon: 'weather-fog'
+    }
+};
+
+function Weather({weatherName, temp}) {
+    return (
+        <LinearGradient 
+        colors={weatherCases[weatherName].colors} 
+        style={styles.container}
+        >
+         <View style={styles.upper}>
+          <MaterialCommunityIcons
+          color="white" 
+          size={144} 
+          name={weatherCases[weatherName].icon}
+           />
+        <Text style={styles.temp}>{temp}℃</Text>
+        </View>
+        <View style={styles.lower}>
+        <Text style={styles.title}>{weatherCases[weatherName].title}</Text>
+        <Text style={styles.subtitle}>
+         {weatherCases[weatherName].subtitle}
+        </Text>
+        </View>
+        </LinearGradient>  
+    );
+}
+
+Weather.propTypes = {
+    temp: PropTypes.number.isRequired,
+    weatherName: PropTypes.string.isRequired
+ };
+
+export default Weather;
+
+const styles = StyleSheet.create({
+    container: {
+    flex:1,
+    },
+    upper: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "transparent",
+        marginTop: 40
+    },
+    temp: {
+        fontSize: 48,
+        backgroundColor: "transparent",
+        color: "white",
+        marginTop: 10
+    },
+    lower: {
+        flex: 1,
+        alignItems: "flex-start",
+        justifyContent: "flex-end",
+        paddingLeft: 25
+    },
+    title: {
+        fontSize: 38,
+        backgroundColor: "transparent",
+        color: "white",
+        marginBottom: 10,
+        fontWeight: "300"
+    },
+    subtitle: {
+        fontSize: 24,
+        backgroundColor: "transparent",
+        color: "white",
+        marginBottom: 40
+    }
+});
